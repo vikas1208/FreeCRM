aa7a8c95aaeff1fb3ba69bfd5a2ba11f0e6de6d3
vikas1208 <48600126+vikas1208@users.noreply.github.com>
2019-04-11 16:30:06 +0530

    Update LoginPageTest.java

--- FreeCRM/src/test/java/com/crm/qa/testcases/LoginPageTest.java
+++ FreeCRM/src/test/java/com/crm/qa/testcases/LoginPageTest.java
@@ -10,7 +10,7 @@ import com.crm.qa.pages.HomePage;
 import com.crm.qa.pages.LoginPage;
 
 public class LoginPageTest extends BaseClass {
-	
+	//Updating LoginTest but in commented area
 	LoginPage loginpage;
 	HomePage homePage;
 	
